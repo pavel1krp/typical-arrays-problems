799356a9c52f99de1dff9cbb32f986104fb3ac4e
pavel1krp <60657881+pavel1krp@users.noreply.github.com>
2020-03-04 18:58:04 +0300

    check

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+  else {
+    array.sort(a, b) = a - b;
+    return array[0];
+  }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+  else {
+    array.sort(a, b) = a - b;
+    return array[array.length - 1];
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  }
+  else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    let sum1 = sum / array.length
+    return sum1;
+  }
 }
